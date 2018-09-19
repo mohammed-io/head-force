a781180509e543fd092006633175ef37e61a8884
Mohammed Abdulkereem <HorizonDragon@outlook.com>
2018-09-19 16:59:53 +0300

    Update

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,9 +10,9 @@ module.exports = {
         name: 'gatsby-starter-default',
         short_name: 'starter',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
-        display: 'minimal-ui',
+        background_color: '#00ff00',
+        theme_color: '#00ff00',
+        display: 'standalone',
         icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
     },
